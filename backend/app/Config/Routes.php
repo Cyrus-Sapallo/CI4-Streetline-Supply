a3af3be52a0b1c5c581132c652b4a11ac8749ce4
Cyrus Sapallo <Sapallow@gmail.com.com>
2026-03-24 15:21:42 +0800

    Revert "fix(routes): update added cart routes"

--- backend/app/Config/Routes.php
+++ backend/app/Config/Routes.php
@@ -51,14 +51,13 @@ $routes->get('cart', 'Cart::index');
 $routes->post('cart/add', 'Cart::add');
 $routes->post('cart/update', 'Cart::update');
 $routes->post('cart/remove', 'Cart::remove');
-$routes->post('cart/buyNow', 'Cart::buyNow');
 
 // Checkout Routes
 $routes->get('checkout', 'Checkout::index');
 $routes->post('checkout/process', 'Checkout::process');
 $routes->get('checkout/success/(:num)', 'Checkout::success/$1');
 
-//wishlist
+
 //wishlist-mark
 $routes->get('wishlist', 'Wishlist::index');
 $routes->get('wishlist/add/(:num)', 'Wishlist::add/$1');
